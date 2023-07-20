b71863e47b41c3efc2d91b8719945a17ab7d420b
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-07-20 11:39:31 +0300

    Assignment Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+
+function EyesOnMe(){
+
+    const gotFocus=()=>{
+        console.log("Good!")      
+    }
+
+    const returnFocus=()=>{
+        console.log("Hey! Eyes on me!")
+       
+    }
+    return(
+        <>
+        <button onFocus={gotFocus} onBlur={returnFocus}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+const inputValue=(event)=>{
+  console.log("Entering password...")
+  console.log(event.target.value)
+}
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password"  onChange={inputValue}></input>
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
 
